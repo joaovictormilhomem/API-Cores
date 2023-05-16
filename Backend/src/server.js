591b856d1831213ccaae9ed671db61e677d58117
João Victor Milhomem <joaovictormilho@gmail.com>
2023-05-16 15:45:37 -0300

    useless code deleted

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,7 +7,6 @@ const routes = require('./routes');
 const app = express();
 app.use(cors());
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 app.listen(process.env.PORT, () => {
